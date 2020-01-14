1bfcf79ef34bf63f9767dda9706ea253b3805337
Sandeep Poman <S.S.Poman@iaea.org>
2020-01-14 16:57:50 +0100

    Adding features to login and register.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import path from 'path';
 import multer from 'multer'
 import canvas from 'canvas'
 import fetch from 'node-fetch';
+import fs from 'fs'
 
 
 
@@ -25,7 +26,23 @@ const storage = multer.diskStorage({
     cb(null, 'Capture.jpg')
   }
 })
-const upload = multer({ storage: storage })
+const uploadSingle = multer({ storage: storage })
+
+const multiStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const dir = `${LABELED_IMAGES_URL}/${req.body.label}`;
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    cb(null, path.join(dir))
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+
+const uploadMultiple = multer({ storage: multiStorage })
 
 
 const { Canvas, Image, ImageData } = canvas;
@@ -50,11 +67,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   return res.send('Received a GET HTTP method');
 });
-app.post('/', upload.single('image'), async (req, res) => {
-
-  console.log(req.file);
-  
-
+app.post('/', uploadSingle.single('image'), async (req, res) => {
   const image = await canvas.loadImage(`${CAPTURED_IMG_URL}`)
 
   const displaySize = { width: image.width, height: image.height }
@@ -65,8 +78,10 @@ app.post('/', upload.single('image'), async (req, res) => {
   return res.send(results);
 });
 
-
-
+app.post('/register', uploadMultiple.array('image'), async (req, res) => {
+  load();
+  return res.send({ label: req.body.label });
+})
 
 
 
@@ -82,8 +97,10 @@ const start = async () => {
   console.log(`Server listening on port ${port}`);
 }
 
+const dirs = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory())
+
 const loadLabeledImages = () => {
-  const labels = ['Sandeep', 'Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+  const labels = dirs(LABELED_IMAGES_URL)
   return Promise.all(
     labels.map(async label => {
 
