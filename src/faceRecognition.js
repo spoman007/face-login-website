8ab8dcfa80a80c85b7be5afd009c254c3482cff3
Sandeep Poman <S.S.Poman@iaea.org>
2020-06-01 16:16:00 +0200

    Updating the pending changes

--- src/faceRecognition.js
+++ src/faceRecognition.js
@@ -2,54 +2,69 @@ import canvas from 'canvas'
 import * as faceapi from 'face-api.js'
 import fs from 'fs'
 import { monkeyPatchFaceApiEnv } from './monkeyPatch'
-import path from 'path';
-import {LABELED_IMAGES_URL, MODELS_URL, CAPTURED_IMG_URL, PORT} from './constants'
+import path from 'path'
+import {
+  LABELED_IMAGES_URL,
+  MODELS_URL,
+  CAPTURED_IMG_URL,
+  PORT,
+} from './constants'
 
-monkeyPatchFaceApiEnv();
+monkeyPatchFaceApiEnv()
 
-let labeledFaceDescriptors = null;
-let faceMatcher = null;
+let labeledFaceDescriptors = null
+let faceMatcher = null
 
-export const load = () => Promise.all([
+export const load = () =>
+  Promise.all([
     faceapi.nets.faceRecognitionNet.loadFromDisk(MODELS_URL),
     faceapi.nets.faceLandmark68Net.loadFromDisk(MODELS_URL),
     faceapi.nets.ssdMobilenetv1.loadFromDisk(MODELS_URL),
     faceapi.nets.ageGenderNet.loadFromDisk(MODELS_URL),
-    faceapi.nets.faceExpressionNet.loadFromDisk(MODELS_URL)
-]).then(start);
+    faceapi.nets.faceExpressionNet.loadFromDisk(MODELS_URL),
+  ]).then(start)
 
 export const identifyFace = async () => {
-    const image = await canvas.loadImage(`${CAPTURED_IMG_URL}`);
-    const displaySize = { width: image.width, height: image.height };
-    const detections = await faceapi.detectSingleFace(image).withFaceLandmarks().withFaceExpressions().withAgeAndGender().withFaceDescriptor();
-    
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    const results = faceMatcher.findBestMatch(resizedDetections.descriptor);
-    return {...results, ...resizedDetections};
-}
+  const image = await canvas.loadImage(`${CAPTURED_IMG_URL}`)
+  const displaySize = { width: image.width, height: image.height }
+  const detections = await faceapi
+    .detectSingleFace(image)
+    .withFaceLandmarks()
+    .withFaceExpressions()
+    .withAgeAndGender()
+    .withFaceDescriptor()
 
+  const resizedDetections = faceapi.resizeResults(detections, displaySize)
+  const results = faceMatcher.findBestMatch(resizedDetections.descriptor)
+  return { ...results, ...resizedDetections }
+}
 
 const start = async () => {
-    labeledFaceDescriptors = await loadLabeledImages();
-    faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-    console.log(`Server listening on port ${PORT}`);
+  labeledFaceDescriptors = await loadLabeledImages()
+  faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+  console.log(`Server listening on port ${PORT}`)
 }
 
-const dirs = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory())
+const dirs = (p) =>
+  fs.readdirSync(p).filter((f) => fs.statSync(path.join(p, f)).isDirectory())
 
 const loadLabeledImages = () => {
-    const labels = dirs(LABELED_IMAGES_URL)
-    return Promise.all(
-        labels.map(async label => {
+  const labels = dirs(LABELED_IMAGES_URL)
+  return Promise.all(
+    labels.map(async (label) => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await canvas.loadImage(
+          `${LABELED_IMAGES_URL}/${label}/${i}.jpg`
+        )
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
 
-            const descriptions = []
-            for (let i = 1; i <= 2; i++) {
-                const img = await canvas.loadImage(`${LABELED_IMAGES_URL}/${label}/${i}.jpg`)
-                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-                descriptions.push(detections.descriptor)
-            }
-
-            return new faceapi.LabeledFaceDescriptors(label, descriptions)
-        })
-    )
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-require('@tensorflow/tfjs-node');
-import express from "express"
-import cors from 'cors';
-import 'dotenv/config';
-import path from 'path';
+require('@tensorflow/tfjs-node')
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import path from 'path'
 import multer from 'multer'
 import fs from 'fs'
-import { load, identifyFace } from "./faceRecognition";
-import { LABELED_IMAGES_URL, TEMP_UPLOAD_FOR_LOGIN_URL, LOGIN_IMG_NAME, PORT } from './constants'
-
-
+import { load, identifyFace } from './faceRecognition'
+import {
+  LABELED_IMAGES_URL,
+  TEMP_UPLOAD_FOR_LOGIN_URL,
+  LOGIN_IMG_NAME,
+  PORT,
+} from './constants'
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, TEMP_UPLOAD_FOR_LOGIN_URL),
-  filename: (req, file, cb) => cb(null, LOGIN_IMG_NAME)
+  filename: (req, file, cb) => cb(null, LOGIN_IMG_NAME),
 })
 const uploadSingle = multer({ storage: storage })
 
 const multiStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const dir = `${LABELED_IMAGES_URL}/${req.body.label}`;
-    !fs.existsSync(dir) ? fs.mkdirSync(dir) : null;
+    const dir = `${LABELED_IMAGES_URL}/${req.body.label}`
+    !fs.existsSync(dir) ? fs.mkdirSync(dir) : null
     cb(null, path.join(dir))
   },
-  filename: (req, file, cb) => cb(null, file.originalname)
+  filename: (req, file, cb) => cb(null, file.originalname),
 })
 
 const uploadMultiple = multer({ storage: multiStorage })
@@ -30,20 +33,18 @@ const uploadMultiple = multer({ storage: multiStorage })
 const app = express()
 app.use(cors())
 app.use(express.static('public'))
-app.use(express.json());
-
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
-});
-app.post('/', uploadSingle.single('image'), async (req, res) => {
-  const results = await identifyFace();
-  return res.send(results);
-});
-app.post('/register', uploadMultiple.array('image'), async (req, res) => {
-  load();
-  return res.send({ label: req.body.label });
+  return res.send('Received a GET HTTP method')
 })
-
+app.post('/', uploadSingle.single('image'), async (req, res) => {
+  const results = await identifyFace()
+  return res.send(results)
+})
+app.post('/register', uploadMultiple.array('image'), async (req, res) => {
+  load()
+  return res.send({ label: req.body.label })
+})
 
 app.listen(PORT, load)
